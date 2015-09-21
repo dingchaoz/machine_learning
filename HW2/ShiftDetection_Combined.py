39f8fa38ba108d7b9a1d3b1498a62eeaedfca212
Dingchao Zhang <ks692@cummins.com>
2015-09-21 12:49:03 -0400

    shiftdetect() is created, will randomly read several text files and output an array with -1 for negative files, and positions of change for positive files

--- HW2/ShiftDetection_Combined.py
+++ HW2/ShiftDetection_Combined.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[26]:
 
 # import libraries
 get_ipython().magic('matplotlib inline')
@@ -42,14 +42,18 @@ import matplotlib.pyplot as plt
 #     data = lines # Otherwise for attribute data, no convert needed     
 # shiftDetect(data)
 
-# In[2]:
+# In[34]:
 
+#### Use Hawkins CUSUM Method described in http://rmgsc.cr.usgs.gov/outgoing/threshold_articles/Hawkins_Zamba2005b.pdf
+#### and in Douglas C. Montgomery Statistical Quality Control 6th Edition Page 400-Page 417 Chapter Time-Weighted Control Charts
+#### Tunable Parameters:
+#### k : Allowance value, a threshold that signals a possible shift starts at the current position
+#### H : If CUSUM value goes beyond this value, conclude a shift has occured and the shift starts at the last point
+####  whose CUSUM is larger than k Allowance value
+#### Seperate k and H values are used for mean and var shift detection
+#### Usage example: data = np.loadtxt("posShiftUpVar_70.txt", delimiter=",") shiftDetect(data)
 
-
-
-
-
-def shiftDetect(data):
+def shiftDetectCont(data):
     
     cusums_mean_up = []   # Ci array for mean upper shift detection
     N_mean_up = [] # N+ array for mean upper shift detection
@@ -162,7 +166,7 @@ def shiftDetect(data):
             plt.plot(cusums_mean_up)
             plt.title("CUSUM of MEAN UP " + str(c_i) + " observations")
             plt.show()
-            return
+            return c_i
         
         if cusum_mean_lo > H:
             
@@ -172,7 +176,7 @@ def shiftDetect(data):
             plt.plot(cusums_mean_lo)
             plt.title("CUSUM of MEAN DOWN" + str(c_i) + " observations")
             plt.show()
-            return
+            return c_i
         
         
         if cusum_var_up > H_v:
@@ -183,7 +187,7 @@ def shiftDetect(data):
             plt.plot(cusums_var_up)
             plt.title("CUSUM of VAR up" + str(c_i) + " observations")
             plt.show()
-            return
+            return c_i
         
         if cusum_var_lo > H_v:
             #c_i = i + n + 1 - (n_var_lo - 1) # The position where change right starts
@@ -193,7 +197,7 @@ def shiftDetect(data):
             plt.plot(cusums_var_lo)
             plt.title("CUSUM of VAR down" + str(c_i) + " observations")
             plt.show()
-            return
+            return c_i
         
        
     print("No Change")
@@ -220,25 +224,151 @@ def shiftDetect(data):
     #plt.plot(all_cusums)
     #plt.title("CUSUM of " + str(n) + " observations")
     #plt.show()
-    return
+    return -1
 
 
 
-# In[30]:
+# In[31]:
 
-## Test with posShiftUpVar_70.txt
+#### Function to get detect concept change for attributes data
+#### Assign char a,b,c to uniformly distributed value seperately for all data points first
+#### Then, group data samples into sub group first,with each group has n data points,calculate the average of each subgroup
+#### Therefore we get a new continous data array named sample
+#### Plot the sample and a shift can be seen obviously
+#### To-do: Experiment using p chart, or Shewart chart or CUSUM or other methods to detect the change position
+#### and in Douglas C. Montgomery Statistical Quality Control 6th Edition Page 400-Page 417 Chapter Time-Weighted Control Charts
+#### Tunable Parameters:
+#### n : subgroup sample size n 
+#### t: The first t data points be selected as baseline
+## posTriple2_160,posTriple_175, posTripleDouble_175, negTriple,negWithRare,negBinary Verified
 
-with open('posShiftUpVar_70.txt') as f: # Open file as f
-    lines = f.read().splitlines()  # Split file line by line and put lines in to var "lines"
-f.close() # Close file
-try:
-    data = [float(i) for i in lines] # For variable dataa stream,convert data string to float if convertable
-except: 
-    data = lines # Otherwise for attribute data, no convert needed
-print(data)
+def shiftDetectAtt(lines):
+    
+    np.random.seed(123456789)
+
+    data = [] # Initiate an array "data" to hold original attributes data 
+    for x in lines:
+        if x == 'a':    #  # Convert char a to a random number from 0 to 1 and append to data array
+            #data.append(0) 
+            data.append(np.random.uniform(0,1,1))
+        if x == 'b':     # Convert char b to a random number from 1 to 2 and append to data array
+            #data.append(1)
+            data.append(np.random.uniform(1,2,1))
+        if x == 'c':    # Convert char c to a random number from 2 to 3 and append to data array
+            #data.append(2)
+            data.append(np.random.uniform(2,3,1))
+        
+
+    n = 14 # Tunable parameter to group each n+1 data points into a new sample point
+    m = len(data)/(n+1) # Number of samples converted from original data
+    sample = [] # A new array to hold continous data converted from attributes data
+
+    ## Fill in the new sample data array converted from original attributes data
+    for i in range(int(m)):
+        if i == 0:
+            sample.append(sum(data[:n])/(n+1))   # Append the average values of each n+1 data points
+        else:
+            sample.append(sum(data[i*n+1:(i+1)*n])/(n+1))  # Append the average values of each n+1 data points
+
+    print("Converted variable values are" + str(sample))
+
+    t = 4 # Choose the first t+1 numbers of sample points as baseline
+
+    ## Calculate control limits using P chart method
+    mean = np.average(sample[:t]) # Get the mean of baseline data
+    sd = np.std(sample[:t])
+    #UCL = mean + 3*np.sqrt(mean*(1-mean)/(n+1)) # Upper control limit in P chart, does not work for negtriple, tripledouble
+    #LCL = mean - 3*np.sqrt(mean*(1-mean)/(n+1)) # Lower control limit in P chart,does not work for negtriple, tripledouble
+    UCL = mean + 3*sd # Upper control limit in Shewart method
+    LCL = mean - 3*sd # Lower control limit in Shewart method
+
+
+    print("mean is in Shewart method:" + str(mean)) # Print the mean value of the control chart
+    print("UCL is in Shewart method: " + str(UCL))  # Print the UCL value of the control chart
+    print("LCL is in Shewart method: " + str(LCL))  # Print the LCL value of the control chart
+    print(str(range(t+1,len(sample))))
+    print(str(sample[5]))
+    
+    #N = [] # Array to record outside threhold points index in a trend
+    n = 0 # index starts from 0 which means not a point outsidte threshold
+    ## Detect if there is concept change occurs after the baseline sample points
+    for i in range(t+1,len(sample)):
+        if (sample[i] >= UCL) | (sample[i] <= LCL): # If there is a point outside threshold
+            n = n + 1
+            #N.append(n)
+            if n == 3: # If it is the consecutive 3rd point that is outside threshold
+                print("Concept change occured at position " + str((i-2)*(n+1)) + "of original attributes data")
+                print("That corresponds to the "+str(i-2)+"th position on the converted data graph below")
+                print("The change occur sample value is"+str(sample[i-2]))
+                plt.plot(sample) # Plot the converted variable smaple data 
+                plt.plot([0, len(sample)], [UCL, UCL], 'r--', color='r')  # Plot the Upper Control Limit
+                plt.plot([0, len(sample)], [LCL, LCL], 'r--', color='r') # Plot the Lower Control LIMIT
+                plt.text(1, UCL - 0.05, 'UCL',color='r') # Label out UCL 
+                plt.text(1, LCL + 0.05, 'LCL',color='r') # Label out LCL 
+                plt.title("Converted Variable Plot from Original Attributes Data,Change Ocurred")
+                plt.show()
+                return i-2
+        else:
+            n = 0
+        
+            
+    
+    print("No Change")
+    plt.plot(sample) # Plot the converted variable smaple data 
+    plt.plot([0, len(sample)], [UCL, UCL], 'r--', color='r')  # Plot the Upper Control Limit
+    plt.plot([0, len(sample)], [LCL, LCL], 'r--', color='r') # Plot the Lower Control LIMIT
+    plt.text(1, UCL - 0.05, 'UCL',color='r') # Label out UCL 
+    plt.text(1, LCL + 0.05, 'LCL',color='r') # Label out LCL 
+    plt.title("Converted Variable Plot from Original Attributes Data")
+    plt.show()
+
+    return -1
+
+
+# In[39]:
+
+## Definition of shiftDetect(data)
+
+def shiftDetect(path):
+    
+    ## Provide a path, and randomly pick 10 txt files to read
+    import os # import os module
+    import random # import random module
+    allfiles = [] # Array to hold all txt files names 
+    for file in os.listdir(path): # For all the files in the current work directory
+        if file.endswith(".txt"):  # If it is a txt file 
+            allfiles.append(file) # Append that file name to allfiles array
+    random.shuffle(allfiles) # Randomly shuffle allfiles
+    n = 13 # Tunable parameter, choose the firt n shuffled files to read
+    files = allfiles[:n] # Choose the first n files
+    output = [] # Output Array
+    
+    for i in range(0,len(files)): # Detect concept change for all the randomly selected files
+        print("Reading " + files[i])
+        with open(files[i]) as f: # Open file as f
+            lines = f.read().splitlines()  # Split file line by line and put lines in to var "lines"
+        f.close() # Close file
+        output.append(files[i])
+        try:
+            data = [float(i) for i in lines] # For variable dataa stream,convert data string to float if convertable
+            shiftDetectCont(data) # Apply continous detection function to see if concept changes
+            output.append(shiftDetectCont(data))
+        except: 
+            data = lines # Otherwise for attribute data, no convert needed
+            shiftDetectAtt(data) # Apply attributes detection function to see if concept changes
+            output.append(shiftDetectAtt(data))
+    
+    print(output)
 
         
-shiftDetect(data)
+        
+    
+
+
+# In[40]:
+
+import os
+shiftDetect(os.getcwd())
 
 
 # In[ ]:
