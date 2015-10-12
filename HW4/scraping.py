ebda245cb4d8a3bb7061f9fe91891369d365691f
pipat <pipat.th@gmail.com>
2015-10-11 21:54:11 -0500

    start working on text scraper

---
+++ HW4/scraping.py
@@ -0,0 +1,83 @@
+import requests
+import re
+import numpy as np
+import pandas as pd
+from threading import Thread
+from queue import Queue
+
+# function to get html text
+def get_html(url):
+    html = requests.get(url)
+    return html.text
+
+# function to get relevant urls from the main page
+def get_urls(main_url):
+    page = get_html(main_url)
+    all_urls = re.findall('<a href="(.*?)"', page, re.DOTALL)
+    return all_urls
+
+# function to get text from each plays url
+def get_plays(url):
+    page = get_html(url)
+    playsname = re.findall('<td class="play" align="center">(.*?)<', page, re.DOTALL)[0].strip() #name of plays
+    act = int(url.rsplit('/', 1)[1].rsplit('.')[1]) #act
+    scene = int(url.rsplit('/', 1)[1].rsplit('.')[2]) #scene
+    title = re.findall('<title>(.*?)<', page, re.DOTALL)[0].strip() #title of work
+    names = []
+    speeches = []
+
+    # get speeches
+    speeches_region = re.split('speech1>', page)[1]
+
+    # get each speech
+    name_speeches = re.findall('<b>(.*?)</blockquote>', speeches_region, re.DOTALL) #name and speech
+    for s in name_speeches:
+        name = re.findall('(.*?)</b>', s, re.DOTALL)[0].strip() #get name
+        raw_speech = re.split('<blockquote>', s)[1] #get all lines
+        lines = re.findall('[0-9]>(.*?)</', raw_speech, re.DOTALL) #get list of lines
+        speech = [line.strip() for line in lines]
+        names.append(name)
+        speeches.append(speech)
+
+    print(len(names), len(speeches))
+
+    # load to pandas df
+    df = pd.DataFrame(columns=['playname', 'title', 'act', 'scene', 'line', 'speaker', 'speech'])
+    for i in range(len(speeches)):
+        df.loc[i] = [playsname, title, act, scene, i, names[i], speeches[i]]
+    print(url + ' load complete')
+    return df
+
+
+
+
+## get all urls
+root = 'http://shakespeare.mit.edu'
+all_main_urls = get_urls(root)
+
+## separate Poetry from Plays
+poetry_main_urls = [url for url in all_main_urls if (url[:6] == 'Poetry')]
+plays_main_urls = [url for url in all_main_urls if (url not in poetry_main_urls)]
+poetry_main_urls = [root + '/' + url for url in poetry_main_urls] #add prefix
+plays_main_urls = [root + '/' + url for url in plays_main_urls] #add prefix
+
+## get urls inside each plays
+plays_all_urls = []
+for url in plays_main_urls:
+    prefix = re.findall('mit.edu/(.*?)/', url)[0]
+    plays_urls = get_urls(url)
+    plays_urls = [url for url in plays_urls if (url[-5:] == '.html' and url != 'full.html')]
+    plays_urls = [root + '/' + prefix + '/' + url for url in plays_urls] #add prefix
+    plays_all_urls.extend(plays_urls) #add to list
+
+
+## get texts from all plays in dataframe
+frames = []
+i = 0
+for url in plays_all_urls:
+    frames.append(get_plays(url))
+    i += 1
+    print(i)
+df = pd.concat(frames, ignore_index=True)
+df.to_csv('plays.txt', sep='\t') #write to tsv
+
